3039ca5125debacaa5e400f3797f95a5f9453b79
naushadraien <naushadraien5@gmail.com>
2023-12-19 15:51:57 +0545

    ci:added admin dashboard graph

---
+++ components/overview.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from "recharts";
+
+interface OverviewProps {
+  data: any[];
+}
+
+const Overview: React.FC<OverviewProps> = ({ data }) => {
+  return (
+    <ResponsiveContainer width="100%" height={350}>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis
+          dataKey="name"
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+        />
+        <YAxis
+          stroke="#888888"
+          fontSize={12}
+          tickLine={false}
+          axisLine={false}
+          tickFormatter={(value) => `$${value}`}
+        />
+        <Bar dataKey="total" fill="#3498db" radius={[4, 4, 0, 0]} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default Overview;
